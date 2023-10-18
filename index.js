4b1b979e5d1e724c9b8fa71d8053f7a701c37fb5
JethroMuluba <jethro.muluba@gmail.com>
2023-10-18 19:27:37 +0100

    creat a Html file

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // 1. Declare a variable firstname and initialize it with the value 'Lata'.
 const firstName = 'Lata';
+ 
+ 	
+// 2. Which value does x have after execution of the following code?
+
+// let x = 'Geeta';
+console.log(x);
