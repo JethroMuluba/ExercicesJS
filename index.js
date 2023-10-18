fd5bccf3644af1ebdb43eb91299471144e31a201
JethroMuluba <jethro.muluba@gmail.com>
2023-10-18 19:57:07 +0100

    response of exercice 5

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // 1. Declare a variable firstname and initialize it with the value 'Lata'.
 const firstName = 'Lata';
- 
  	
 // 2. Which value does x have after execution of the following code? let x = 'Geeta';
 
@@ -11,10 +10,19 @@ const firstName = 'Lata';
 // 3. Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
 const flower = 'rose';
 const tree = 'maple';
-// 4. Which value does x have after execution of the following code?
 
-let x = 'Tic';
-x = 'Tac';
-x = 'Toe';
-console.log(x);
+// 4. Which value does x have after execution of the following code?
+// let x = 'Tic';
+// x = 'Tac';
+// x = 'Toe';
+// console.log(x);
 // after execution of this code x will have 'Toe' like value
+
+// 5. Which value does x have after execution of the following code?                            
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z;
+console.log(x);
+// after execution of this code x will have 'Hardy' like value
