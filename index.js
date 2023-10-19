66df74422c8ea0e6864d5bcbd5db6d8e333846d1
JethroMuluba <jethro.muluba@gmail.com>
2023-10-19 20:36:14 +0100

    Responde if the response id true or false

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 1. Declare a variable firstname and initialize it with the value 'Lata'.
-const firstName = 'Lata';
+// const firstName = 'Lata';
  	
 // 2. Which value does x have after execution of the following code? let x = 'Geeta';
 
@@ -8,8 +8,8 @@ const firstName = 'Lata';
 // Output: Geeta
 
 // 3. Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
-const flower = 'rose';
-const tree = 'maple';
+// const flower = 'rose';
+// const tree = 'maple';
 
 // 4. Which value does x have after execution of the following code?
 // let x = 'Tic';
@@ -19,30 +19,67 @@ const tree = 'maple';
 // after execution of this code x will have 'Toe' like value
 
 // 5. Which value does x have after execution of the following code?                            
-let x = 'Laurel';
-let y = 'Hardy';
-let z = y;
-y = x;
-x = z;
-console.log(x);
+// let x = 'Laurel';
+// let y = 'Hardy';
+// let z = y;
+// y = x;
+// x = z;
+// console.log(x);
 // after execution of this code x will have 'Hardy' like value
 
 // 6.  Define a function hello that returns 'Hello world!'
-function hello() {
-    return 'Hello world!';
-};
+// function hello() {
+//     return 'Hello world!';
+// };
 // 7. Define two functions. The first function a should return 'Hello a!' and the second function b should return 'Hello b!'.
-function firstFunction() {
-    return 'Hello a!';
-};
-function secondFunction() {
-    return 'Hello b!';
-};
+// function firstFunction() {
+//     return 'Hello a!';
+// };
+// function secondFunction() {
+//     return 'Hello b!';
+// };
 
 // 8 Define a function greet returning the value  'Haydo!'   
 // Declare a variable salutation Call the function greet and assign the result of the call to the variable salutation.
-function greet(){
-    return 'Haydo!';
-}
-const salutation = greet();
-console.log(salutation);
+// function greet(){
+//     return 'Haydo!';
+// }
+// const salutation = greet();
+// console.log(salutation);
+// 9. Define a function hello returning the value  'Hi!'
+// Declare a variable x Call the function hello and assign the result of the call to the variable x.
+// function hello() {
+//     return 'Hi!';
+//   };
+  
+//   let x = hello();
+//   console.log(x);
+// 10. Write a function echo that also returns the passed parameter. echo('Greta') 
+// should return 'Greta' and echo('CO2') should return 'CO2'
+// function echo(Greta){
+//     return 'Greta';
+// };
+// function echo(CO2){
+//     return 'CO2';
+// };
+
+
+// Second part of JavaScript exercice
+// Number 1
+let a = 0, 
+    b = 1, 
+    c = '1';
+
+// Que vaut (a === 0); R/ true         
+// Que vaut (a === 1); R/ false
+// Que vaut (a === b); R/ false
+// Que vaut (a === 0 && b === 1); R/ true
+// Que vaut (a === 0 && b === 0); R/ false
+// Que vaut (a === 0 || b === 0); R/ false
+// Que vaut (b === c); R/ false
+// Que vaut (b == c); R/ false
+// Que vaut (a < b);  R/ true
+// Que vaut (a >= b);  R/ false
+// Que vaut (a !== b); R/ true
+// Que vaut (a === 0 && (b === 1 || b === 2)) R/ false
+// Que vaut (a === 0 && true) R/ false
